823586c6fbf55cb9551b379c68bc558df3481bf3
arxyus03 <xyrilljohnabreu25@gmail.com>
2025-11-14 17:43:22 +0800

    update sa admin

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import Linking from 'Linking.jsx'
+import Linking from './Pages/Linking.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
